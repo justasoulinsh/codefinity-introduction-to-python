4296e630bce0bec64b6a8c966fb0bc36c6c6daac
justasoulinsh <>
2026-02-21 17:19:52 +0000

    Variable Naming

--- variables_and_types/variable_naming/main.py
+++ variables_and_types/variable_naming/main.py
@@ -2,6 +2,11 @@
 
 
 # Testing
-print("Item:", ___)
-print("Price per loaf: $", ___)
-print("Quantity in stock:", ___)
+item_name = "Bread"
+print("Item:", item_name)
+
+item_price = 4.52
+print("Price per loaf: $", item_price)
+
+items_in_stock = 230
+print("Quantity in stock:", items_in_stock)
